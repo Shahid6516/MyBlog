1ffb79395903e77349df0d0cc52d061a565a622b
Mohammad Shahid <www.shahid6516@gmail.com>
2025-10-01 11:42:04 +0530

    MyBlog setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import cors from "cors"
+
+// const router = express.Router();
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT, () => {
+  connectDb();
+  console.log(`Server is runing on PORT:${process.env.PORT}`);
+});
